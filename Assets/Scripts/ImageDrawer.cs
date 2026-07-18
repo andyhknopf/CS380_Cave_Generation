b150aba1c17cf5f800cdef36e7665ecf460bbec8
luisjan0411 <luisjan0411@gmail.com>
2026-07-17 18:41:30 -0700

    noise map generation 2d and 3d(haven't find a way to visualize it yet)

--- Assets/Scripts/ImageDrawer.cs
+++ Assets/Scripts/ImageDrawer.cs
@@ -8,18 +8,29 @@ public class ImageDrawer : MonoBehaviour
         WorleyMoise
     }
 
+    public enum NoiseDimension
+    {
+        Noise2D,
+        Noise3D
+    }
+
+
+
     private Texture2D texture;
 
     //later change the resolution to match the size of our voxel grid
     const int textureResolution = 256;
-    const int gridResolution = 8;
+    const int gridResolution = 12;
 
-    Vector2[,] perlinPoints;
-    Vector2[,] worleyPoints;
+    Vector2[,] perlinPoints2D;
+    Vector2[,] worleyPoints2D;
 
-    float[,] finalNoiseMap;
+    float[,] finalNoiseMap2D;
+    float[,,] finalNoiseMap3D;
 
-    public ImageDrawer.NoiseType noiseType = ImageDrawer.NoiseType.PerlinNoise;
+    [SerializeField]
+    public NoiseType noiseType = NoiseType.PerlinNoise;
+    private NoiseDimension noiseDimension = NoiseDimension.Noise2D;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -28,12 +39,20 @@ public class ImageDrawer : MonoBehaviour
         texture = new Texture2D(textureResolution, textureResolution, TextureFormat.RGBA32, false);
         GetComponent<Renderer>().material.SetTexture("_BaseMap", texture);
 
-        finalNoiseMap = new float[textureResolution, textureResolution];
+        finalNoiseMap2D = new float[textureResolution, textureResolution];
+        finalNoiseMap3D = new float[textureResolution, textureResolution, textureResolution];
 
         switch (noiseType)
         {
             case NoiseType.PerlinNoise:
-                PerlinNoise();
+                if (noiseDimension == NoiseDimension.Noise2D)
+                {
+                    PerlinNoise2D();
+                }
+                else
+                {
+                    PerlinNoise3D();
+                }
                 break;
             case NoiseType.WorleyMoise:
                 WorleyMoise();
@@ -49,9 +68,9 @@ public class ImageDrawer : MonoBehaviour
     //}
 
     //used the algorithm from the tutorial https://www.youtube.com/watch?v=9B89kwHvTN4 
-    void PerlinNoise() 
+    void PerlinNoise2D() 
     {
-        perlinPoints = new Vector2[
+        perlinPoints2D = new Vector2[
             gridResolution + 1,
             gridResolution + 1
         ];
@@ -64,7 +83,7 @@ public class ImageDrawer : MonoBehaviour
                 float pointRotation = Random.Range(0f, 360f);
                 float radians = pointRotation * Mathf.Deg2Rad;
 
-                perlinPoints[x, y] = new Vector2(
+                perlinPoints2D[x, y] = new Vector2(
                     Mathf.Cos(radians),
                     Mathf.Sin(radians)
                 );
@@ -79,26 +98,61 @@ public class ImageDrawer : MonoBehaviour
 
                 float sampleY = (float)y / textureResolution * gridResolution;
 
-                float noiseValue = CalculatePerlinPoint(
-                    sampleX,
-                    sampleY
-                );
+                float noiseValue = CalculatePerlinPoint(sampleX, sampleY);
 
                 // this brightness is the final value of the pixel, which is a float between 0 and 1
                 // we can use this value to create our voxel
                 float brightness = noiseValue * 0.5f + 0.5f;
 
-                finalNoiseMap[x, y] = brightness;
+                finalNoiseMap2D[x, y] = brightness;
 
-                Color color = new Color(
-                    brightness,
-                    brightness,
-                    brightness
-                );
+                Color color = new Color(brightness, brightness, brightness );
 
                 texture.SetPixel(x, y, color);
             }
         }
+    }
+
+    void PerlinNoise3D()
+    {
+        perlinPoints2D = new Vector2[
+            gridResolution + 1,
+            gridResolution + 1
+        ];
+
+        for (int x = 0; x <= gridResolution; x++)
+        {
+            for (int y = 0; y <= gridResolution; y++)
+            {
+                float pointRotation = Random.Range(0f, 360f);
+                float radians = pointRotation * Mathf.Deg2Rad;
+
+                perlinPoints2D[x, y] = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians));
+            }
+        }
+
+        for (int x = 0; x < textureResolution; x++)
+        {
+            for (int y = 0; y < textureResolution; y++)
+            {
+                for (int z = 0; z < textureResolution; z++)
+                {
+                    float sampleX = (float)x / textureResolution * gridResolution;
+
+                    float sampleY = (float)y / textureResolution * gridResolution;
+
+                    float sampleZ = (float)z / textureResolution * gridResolution;
+
+                    float noiseValue = CalculateApproximatePerlin3D(sampleX, sampleY, sampleZ);
+
+                    float brightness = noiseValue * 0.5f + 0.5f;
+
+                    finalNoiseMap3D[x, y, z] = Mathf.Clamp01(brightness);
+                }
+            }
+
+        }
+
     }
 
     float CalculatePerlinPoint(float sampleX, float sampleY)
@@ -109,10 +163,10 @@ public class ImageDrawer : MonoBehaviour
         int x1 = x0 + 1;
         int y1 = y0 + 1;
 
-        Vector2 bottomLeftGradient = perlinPoints[x0, y0];
-        Vector2 bottomRightGradient = perlinPoints[x1, y0];
-        Vector2 topLeftGradient = perlinPoints[x0, y1];
-        Vector2 topRightGradient = perlinPoints[x1, y1];
+        Vector2 bottomLeftGradient = perlinPoints2D[x0, y0];
+        Vector2 bottomRightGradient = perlinPoints2D[x1, y0];
+        Vector2 topLeftGradient = perlinPoints2D[x0, y1];
+        Vector2 topRightGradient = perlinPoints2D[x1, y1];
 
         Vector2 point = new Vector2(sampleX, sampleY);
 
@@ -129,25 +183,27 @@ public class ImageDrawer : MonoBehaviour
         float percentX = Fade(sampleX - x0);
         float percentY = Fade(sampleY - y0);
 
-        float bottomValue = Mathf.Lerp(
-            bottomLeftValue,
-            bottomRightValue,
-            percentX
-        );
+        float bottomValue = Mathf.Lerp(bottomLeftValue, bottomRightValue, percentX);
 
-        float topValue = Mathf.Lerp(
-            topLeftValue,
-            topRightValue,
-            percentX
-        );
+        float topValue = Mathf.Lerp(topLeftValue, topRightValue, percentX);
 
-        float finalValue = Mathf.Lerp(
-            bottomValue,
-            topValue,
-            percentY
-        );
+        float finalValue = Mathf.Lerp(bottomValue, topValue, percentY);
 
         return finalValue;
+    }
+
+    //used the algorithm from the tutorial https://www.youtube.com/watch?v=TZFv493D7jo&t=8s
+    float CalculateApproximatePerlin3D(float sampleX, float sampleY, float sampleZ)
+    {
+        float xy = CalculatePerlinPoint(sampleX, sampleY);
+        float yz = CalculatePerlinPoint(sampleY, sampleZ);
+        float xz = CalculatePerlinPoint(sampleX, sampleZ);
+
+        float yx = CalculatePerlinPoint(sampleY, sampleX);
+        float zy = CalculatePerlinPoint(sampleZ, sampleY);
+        float zx = CalculatePerlinPoint(sampleZ, sampleX);
+
+        return (xy + yz + xz + yx + zy + zx) / 6f;
     }
 
     float Fade(float t)
@@ -169,17 +225,19 @@ public class ImageDrawer : MonoBehaviour
         //        float randomX = Random.Range(0f, 1f);
         //        float randomY = Random.Range(0f, 1f);
 
-        //        worleyPoints[x, y] = new Vector2(
-        //            x + randomX,
-        //            y + randomY
-        //        );
+        //        worleyPoints[x, y] = new Vector2(x + randomX, y + randomY);
         //    }
         //}
     }
 
     // this map store the final noise values for each pixel, which we can use to create our voxel
-    public float[,] GetNoiseMap()
+    public float[,] GetNoiseMap2D()
     {
-        return finalNoiseMap;
+        return finalNoiseMap2D;
+    }
+
+    public float[,,] GetNoiseMap3D()
+    {
+        return finalNoiseMap3D;
     }
 }
